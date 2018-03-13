c9cb023e5891aafe1dd8cb07888abb7af12fad1b
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-03-13 03:27:31 +0100

    Update Model Book

--- db/migrate/20180313015309_create_books.rb
+++ db/migrate/20180313015309_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
     create_table :books do |t|
       t.string :title
       t.string :author
-      t.string :caterogy
+      t.string :category
       t.string :cover_url
 
       t.timestamps
