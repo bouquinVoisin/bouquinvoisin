3dcde9547136d7d54ce0fc9e47bd718d1a90ec10
Leila Chaouih <leila_06@hotmail.com>
2018-03-13 20:21:51 +0800

    Create view Show Users

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
 
   get 'books/new'
 
+  
   devise_for :users
+    resources :users, :only => [:show, :profile]
+
+
+
   root 'static_pages#home'
 
   get 'static_pages/about'
