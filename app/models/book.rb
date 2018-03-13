7b36a5590b4296056cc0229051ad923888477e3c
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-03-13 03:07:51 +0100

    Update Model Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
 	has_many :users, :through => :reviews
-	has_many :reviews
+	has_many :reviews, dependent: :destroy
 
 	validates :title, :author, :category, presence: true
 end
