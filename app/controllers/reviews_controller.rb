8501ba5baa32352e59c19c9d662ed36781280bb8
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-03-13 12:05:07 +0100

    Update Design

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -34,6 +34,7 @@ class ReviewsController < ApplicationController
   def update
     @review = Review.find(params[:id])
     if @review.update(review_params)
+      flash[:success] = "Vous avez bien édité votre commentaire"
       redirect_to @review
   	else render 'edit'
     end
