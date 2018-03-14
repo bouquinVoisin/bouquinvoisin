cc6027f5d58559fd3f15d8068f2707d0d2690732
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-03-14 07:02:23 +0100

    Add config email production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,18 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            "GMAIL_USERNAME",
+    password:             "GMAIL_PASSWORD",
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
+  config.action_mailer.default_url_options = { host: "http://bouquinvoisin.herokuapp.com/" }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
