119eb23c7c059e0644c4f2e0fa5c717064e17cb8
sebastienrombaut <sebastien.rombaut@hotmail.fr>
2018-03-20 09:56:16 +0100

    facebook login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319071156) do
+ActiveRecord::Schema.define(version: 20180320074334) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20180319071156) do
     t.float "longitude"
     t.string "postal_code"
     t.string "avatar"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
